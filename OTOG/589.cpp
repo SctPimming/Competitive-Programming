5e14e4d9939ea64ed5a139cdb1678cd71fee460a
Amethyst <39304441+SctPimming@users.noreply.github.com>
2019-03-14 19:49:15 +0700

    create solution (ad-hoc)

---
+++ OTOG/589.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define rep(i,k,n) for(int i=(k);i!=int(n);++i)
+#define sz(x) x.size()
+#define all(x) x.begin(),x.end()
+#define dbg(x) cout << "debug :" << x << endl;
+#define dbg2(x,y) cout << "debug :" << x << ":" << y << endl;
+#define input() (*istream_iterator<int>(cin))
+using namespace std;
+
+int my_mul(int a, int b){
+  return a*b;
+}
+int my_div(int a, int b){
+  return a/b;
+}
+int my_add(int a, int b){
+  return a+b;
+}
+int my_sub(int a, int b){
+  return a-b;
+}
