8be914fc23656b4aefb48cc197389b1d461af694
Amethyst <39304441+SctPimming@users.noreply.github.com>
2019-03-14 19:31:34 +0700

    create solution (cmp function implement)

---
+++ OTOG/599.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define rep(i,k,n) for(int i=(k);i!=int(n);++i)
+#define sz(x) x.size()
+#define all(x) x.begin(),x.end()
+#define dbg(x) cout << "debug :" << x << endl;
+#define dbg2(x,y) cout << "debug :" << x << ":" << y << endl;
+#define input() (*istream_iterator<int>(cin))
+
+using namespace std;
+using pii = pair<int, int>;
+
+bool cmp(pii a, pii b){
+  if(a.fi == b.fi) return a.se > b.se;
+  else return a.fi < b.fi;
+}
+
+int  main(){
+  int n = input();
+  std::vector<pii> v(n,{0, 0});
+  rep(i, 0, n) cin >> v[i].fi >> v[i].se;
+  sort(all(v), cmp);
+  for(pii cur : v) cout << cur.fi << " " << cur.se << "\n";
+}
